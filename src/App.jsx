ebda42b1d361392030e4c5bb710b0fde67b14513
AhmadTash <144666528+AhmadTash@users.noreply.github.com>
2024-06-16 16:12:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import './App.css'
 import Navbar from './components/partials/Navbar'
 import LandingPage from './components/LandingPage'
 import elipse from './assets/Ellipse1.svg'
-import useFetch from './hooks/useFetch'
+
 
 function App() {
-  const { loading, error, data } = useFetch('http://localhost:1337/api/cars')
+  
 
 
-  if (loading) {
-    return <h1>Loading...</h1>
-  }
-  if (error) {
-    return <h1>Error: {error.message}</h1>
-  }
+  
   return (
     <>
     <img src={elipse} alt="" style={{position: "absolute", top: "120px"}}/>
